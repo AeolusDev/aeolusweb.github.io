d9522444b0c171fae2fb8e9fbe5224b00d37746c
Aeolus <aeolusdeveloper@gmail.com>
2023-12-05 00:47:47 +0530

    Added JS for birtday display in about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,6 +163,31 @@
     })
   }
 
+  
+  /**
+   * Birthday Display
+   */
+
+  function calculateYearsSince(dateString) {
+    const pastDate = new Date(dateString);
+
+    const currentDate = new Date();
+  
+    const timeDifference = currentDate - pastDate;
+  
+    const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+  
+    // Calculate the number of years
+    const years = timeDifference / millisecondsInYear;
+  
+    return years;
+  }
+  
+  // Example: Calculate years since January 1, 2000
+  const age = calculateYearsSince('2007-09-01');
+  const roundedAge = Math.floor(age);
+  document.getElementById('birthday').innerHTML = roundedAge;
+
   /**
    * Porfolio isotope and filter
    */
